7c4f8ba0573d9c91a912a1885e6cee7f9a424b4c
กมลรัตน์ ลาภผล <s6002041520018@Macmini-213-14.local>
2019-03-14 16:35:52 +0700

    create App component

---
+++ components/App.js
@@ -0,0 +1,14 @@
+//import library
+import React from 'react';
+import { View, Text } from 'react-native'
+class App extends React.Components {
+    render() {
+        return (
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        );
+    }
+}
+//export
+export default App;
