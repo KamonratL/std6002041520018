f3cec7c7eaf610fb374a15b96ba2b30474fdeaa2
กมลรัตน์ ลาภผล <s6002041520018@Macmini-213-14.local>
2019-03-28 14:52:36 +0700

    using Header component on App

--- components/App.js
+++ components/App.js
@@ -1,7 +1,7 @@
 //import library
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import Header from './Header'
+import Header from './Header';
 
 class App extends Component  {
     render() {
